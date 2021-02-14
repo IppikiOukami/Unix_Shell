d547cc98651cb74da71bf001ae45a8d1a057a5ad
ippikioukami <memacias2@miners.utep.edu>
2021-02-14 16:30:45 -0700

    broke into separate operations to get commands working

--- shell/shell.py
+++ shell/shell.py
@@ -1,51 +1,43 @@
 import os, sys, re
 
 def runShell():
-    os.environ['PS1'] = '$'                                                  #change PS1 to $
-    print(os.environ['PS1'],end='')                                          #display to console
-    usIn = readLine()                                                        #capture input
-    args = usIn.split(" ")                                                   #split input on ' '
-    if usIn == 'exit': sys.exit(0)                                           #exit on command
-    rc = os.fork()                                                           #attempt fork()
-    if rc < 0:                                                               #Failed fork
-        os.write(2, ('Fork failed, returning {}\n'.format(rc)).encode())
-        sys.exit(1)
-    elif rc == 0:
-        for dir in re.split(':',os.envrion['PATH']):
-            program = '{}/{}'.format(dir,args[0])
-            try: os.execve(program,args,os.environ)
+    while True:                                                     #keep active prompt
+        pid = os.getpid()
+        if 'PS1' in os.environ: os.write(1, (os.eviron['PS1']).encode())
+        else:
+            os.write(1,('$ ').encode())                             #use PS1 if present
+            
+        usIN = input()                                              #get input
+        usIN = usIN.split(' ')                                      #delimit input by spacing
+
+        if 'cd' in usIN:                                            #change dir command
+            try: os.chdir(usIN[1])
             except FileNotFoundError: pass
-        os.write(2, ('Child: Could not exec {}\n'.format(args[0]).encode()))
+            continue
+        if 'exit' in usIN: sys.exit(0)                              #exit on command
+
+        if not usIN: pass                                           #try getting input again
+        else: runCommand(usIN,pid)                                  #inspect input
+                    
+def myExe(args):                                                    #attempt to exec
+    for dir in re.split(':', os.environ["PATH"]):
+        program = '{}/{}'.format(dir,args[0])
+        try: os.execve(program, args, os.environ)
+        except FileNotFoundError: pass                              #quiet fail
+    os.write(2,('{}: command not found'.format(args[0])).encode())  #notify user
+    sys.exit(1)
+
+def runCommand(args,pid=None):                                      #find command
+    rc = os.fork()                                                  #attempt fork
+    if rc < 0:                                                      #Failed fork
+        os.write(2, ('Fork Failed, returning {}\n'.format(rc)).encode())
         sys.exit(1)
-    else:                                                                    #Parent Process
-        cPID = os.wait()
-        os.write(1, ('Parent: Child {} terminated with exit code {}\n'
-                     .format(cPID[0],cPID[1]).encode()))
-        runShell()
+    elif rc == 0:                                                   #Child process
+        myExe(args)
+    else:                                                           #Parent process
+        if not '&' in args:
+            cPID = os.wait()
+            os.write(1, ('Parent: Child {} terminated with exit code {}\n'.format(cPID[0],cPID[1])).encode())
 
-raw = None
-buf = ''
 
-def readLine():
-    global buf
-
-    while True:
-        line = getChar()
-        if char == '': return ''
-        if char == '\n':
-            outLine, buf = buf, ''
-            return outLine
-        else: buf += char
-
-def getChar():
-    global raw, buf
-
-    if not raw or not len(buf):
-        raw = read(0,100)
-        buf = raw.decode()
-    if len(buf):
-        outChar, buf = buf[0], buf[1:]
-        return outChar
-    else: return ''
-                 
 runShell()
